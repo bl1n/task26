1ec61721c1ef9aa74af85b665fd41dd45437c81f
bl1n <sukhanov87@gmail.com>
2019-07-12 15:41:10 +0300

    work, work, and another one

--- app/src/main/java/com/lft/task26/MainActivity.java
+++ app/src/main/java/com/lft/task26/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.util.DisplayMetrics;
+import android.util.EventLogTags;
+import android.util.TypedValue;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +19,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,91 +27,171 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-        final int dp = (int)metrics.density;
-        final float sp = metrics.scaledDensity;
 
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-
-            FrameLayout root = new FrameLayout(this);
-            root.setPadding(16 * dp, 16 * dp , 16 * dp, 16 * dp );
-            FrameLayout.LayoutParams frameParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-            root.setLayoutParams(frameParams);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+            initPortraitUI();
+        else
+            initLandscapeUI();
 
 
-            RelativeLayout relativeLayout = new RelativeLayout(this);
-            RelativeLayout.LayoutParams relParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
 
-            root.addView(relativeLayout,relParams );
-
-            ImageView ivIcon = new ImageView(this);
-            ivIcon.setId(11);
-            ivIcon.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            ivIcon.setImageResource(R.mipmap.ic_launcher);
-            ViewGroup.LayoutParams linParams = new ViewGroup.LayoutParams(192*dp,128*dp);
-            relativeLayout.addView(ivIcon, linParams);
-
-
-            TextView tvTitle = new TextView(this);
-            tvTitle.setId(12);
-            tvTitle.setText(getResources().getString(R.string.elafonisy_beach));
-            tvTitle.setTextSize(28 * sp);
-            tvTitle.setTypeface(null, Typeface.BOLD);
-            RelativeLayout.LayoutParams tvTitleParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            tvTitleParams.setMargins(8 * dp, 0, 0, 0);
-            tvTitleParams.setMarginStart(8 * dp);
-            tvTitleParams.addRule(RelativeLayout.ALIGN_TOP, ivIcon.getId());
-            tvTitleParams.addRule(RelativeLayout.END_OF, ivIcon.getId());
-            tvTitleParams.addRule(RelativeLayout.RIGHT_OF, ivIcon.getId());
-            relativeLayout.addView(tvTitle,tvTitleParams );
+    private void initLandscapeUI() {
+        FrameLayout root = new FrameLayout(this);
+        root.setPadding(getResources().getDimensionPixelSize(R.dimen.sixteen_dp),
+                getResources().getDimensionPixelSize(R.dimen.sixteen_dp),
+                getResources().getDimensionPixelSize(R.dimen.sixteen_dp),
+                getResources().getDimensionPixelSize(R.dimen.sixteen_dp));
+        FrameLayout.LayoutParams frameParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        root.setLayoutParams(frameParams);
 
 
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams relParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        root.addView(relativeLayout, relParams);
 
-            TextView tvQuote = new TextView(this);
-            tvQuote.setText(getResources().getText(R.string.description));
-            tvQuote.setTypeface(null, Typeface.ITALIC);
-            tvQuote.setTextSize(14 * sp);
-            RelativeLayout.LayoutParams tvQuoteParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-//            tvQuoteParams.addRule(RelativeLayout.ALIGN_BOTTOM, ivIcon.getId());
-            tvQuoteParams.addRule(RelativeLayout.ALIGN_LEFT, tvTitle.getId());
-            tvQuoteParams.addRule(RelativeLayout.ALIGN_START, tvTitle.getId());
-            tvQuoteParams.addRule(RelativeLayout.BELOW, tvTitle.getId());
-            tvQuoteParams.setMargins(0, 8 * dp, 0, 0);
-            relativeLayout.addView(tvQuote, tvQuoteParams);
-
-            EditText etComment = new EditText(this);
-            etComment.setInputType(InputType.TYPE_CLASS_TEXT);
-            etComment.setId(111);
-            RelativeLayout.LayoutParams etCommentParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            etCommentParams.setMargins(0, 8 * dp, 0, 0);
-            etComment.setHint(getResources().getString(R.string.etcomment));
-            etComment.setTextSize(14 * sp);
-            etCommentParams.addRule(RelativeLayout.BELOW, ivIcon.getId());
-            etComment.setLayoutParams(etCommentParams);
-            relativeLayout.addView(etComment);
+        ImageView ivIcon = new ImageView(this);
+        ivIcon.setId(11);
+        ivIcon.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        ivIcon.setImageResource(R.mipmap.ic_launcher);
+        ViewGroup.LayoutParams linParams = new ViewGroup.LayoutParams(getResources().getDimensionPixelSize(R.dimen.ivWeight), getResources().getDimensionPixelSize(R.dimen.ivHeight));
+        relativeLayout.addView(ivIcon, linParams);
 
 
-            LinearLayout linearLayout = new LinearLayout(this);
-            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            layoutParams.addRule(RelativeLayout.BELOW, etComment.getId());
-            linearLayout.setLayoutParams(layoutParams);
-            relativeLayout.addView(linearLayout);
-
-            Button btnPrevious = new Button(this);
-            LinearLayout.LayoutParams btnPreviousParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1f);
-            btnPrevious.setLayoutParams(btnPreviousParams);
-            btnPrevious.setText(getResources().getString(R.string.btn_previous));
-            linearLayout.addView(btnPrevious);
-
-            Button btnNext = new Button(this);
-            LinearLayout.LayoutParams btnNextParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1f);
-            btnNext.setLayoutParams(btnNextParams);
-            btnNext.setText(getResources().getString(R.string.btn_next));
-            linearLayout.addView(btnNext);
-
-            setContentView(root);
-        }
+        TextView tvTitle = new TextView(this);
+        tvTitle.setId(12);
+        tvTitle.setText(getResources().getString(R.string.elafonisy_beach));
+        tvTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28);
+        tvTitle.setTypeface(null, Typeface.BOLD);
+        RelativeLayout.LayoutParams tvTitleParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        tvTitleParams.setMargins(getResources().getDimensionPixelSize(R.dimen.eight_dp), 0, 0, 0);
+        tvTitleParams.setMarginStart(getResources().getDimensionPixelSize(R.dimen.eight_dp));
+        tvTitleParams.addRule(RelativeLayout.ALIGN_TOP, ivIcon.getId());
+        tvTitleParams.addRule(RelativeLayout.END_OF, ivIcon.getId());
+        relativeLayout.addView(tvTitle, tvTitleParams);
 
 
+        TextView tvQuote = new TextView(this);
+        tvQuote.setText(getResources().getText(R.string.description));
+        tvQuote.setTypeface(null, Typeface.ITALIC);
+        tvQuote.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        RelativeLayout.LayoutParams tvQuoteParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        tvQuoteParams.addRule(RelativeLayout.ALIGN_BOTTOM, ivIcon.getId());
+        tvQuoteParams.addRule(RelativeLayout.ALIGN_START, tvTitle.getId());
+        tvQuoteParams.addRule(RelativeLayout.BELOW, tvTitle.getId());
+        tvQuoteParams.setMargins(0, getResources().getDimensionPixelSize(R.dimen.eight_dp), 0, 0);
+        relativeLayout.addView(tvQuote, tvQuoteParams);
+
+        EditText etComment = new EditText(this);
+        etComment.setInputType(InputType.TYPE_CLASS_TEXT);
+        etComment.setId(111);
+        RelativeLayout.LayoutParams etCommentParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        etCommentParams.setMargins(0, getResources().getDimensionPixelSize(R.dimen.eight_dp), 0, 0);
+        etComment.setHint(getResources().getString(R.string.etcomment));
+        etComment.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        etCommentParams.addRule(RelativeLayout.BELOW, ivIcon.getId());
+        etComment.setLayoutParams(etCommentParams);
+        relativeLayout.addView(etComment);
+
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.BELOW, etComment.getId());
+        linearLayout.setLayoutParams(layoutParams);
+        relativeLayout.addView(linearLayout);
+
+        Button btnPrevious = new Button(this);
+        LinearLayout.LayoutParams btnPreviousParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+        btnPrevious.setLayoutParams(btnPreviousParams);
+        btnPrevious.setText(getResources().getString(R.string.btn_previous));
+        linearLayout.addView(btnPrevious);
+
+        Button btnNext = new Button(this);
+        LinearLayout.LayoutParams btnNextParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+        btnNext.setLayoutParams(btnNextParams);
+        btnNext.setText(getResources().getString(R.string.btn_next));
+        linearLayout.addView(btnNext);
+
+        setContentView(root);
+    }
+
+    private void initPortraitUI() {
+        FrameLayout root = new FrameLayout(this);
+        root.setPadding(getResources().getDimensionPixelSize(R.dimen.sixteen_dp),
+                getResources().getDimensionPixelSize(R.dimen.sixteen_dp),
+                getResources().getDimensionPixelSize(R.dimen.sixteen_dp),
+                getResources().getDimensionPixelSize(R.dimen.sixteen_dp));
+        FrameLayout.LayoutParams frameParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        root.setLayoutParams(frameParams);
+
+
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        RelativeLayout.LayoutParams relParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        root.addView(relativeLayout, relParams);
+
+        ImageView ivIcon = new ImageView(this);
+        ivIcon.setId(11);
+        ivIcon.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        ivIcon.setImageResource(R.mipmap.ic_launcher);
+        ViewGroup.LayoutParams linParams = new ViewGroup.LayoutParams(getResources().getDimensionPixelSize(R.dimen.ivWeight), getResources().getDimensionPixelSize(R.dimen.ivHeight));
+        relativeLayout.addView(ivIcon, linParams);
+
+
+        TextView tvTitle = new TextView(this);
+        tvTitle.setId(12);
+        tvTitle.setText(getResources().getString(R.string.elafonisy_beach));
+        tvTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, 28);
+        tvTitle.setTypeface(null, Typeface.BOLD);
+        RelativeLayout.LayoutParams tvTitleParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        tvTitleParams.setMargins(getResources().getDimensionPixelSize(R.dimen.eight_dp), 0, 0, 0);
+        tvTitleParams.setMarginStart(getResources().getDimensionPixelSize(R.dimen.eight_dp));
+        tvTitleParams.addRule(RelativeLayout.ALIGN_TOP, ivIcon.getId());
+        tvTitleParams.addRule(RelativeLayout.END_OF, ivIcon.getId());
+        relativeLayout.addView(tvTitle, tvTitleParams);
+
+
+        TextView tvQuote = new TextView(this);
+        tvQuote.setText(getResources().getText(R.string.description));
+        tvQuote.setTypeface(null, Typeface.ITALIC);
+        tvQuote.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        RelativeLayout.LayoutParams tvQuoteParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        tvQuoteParams.addRule(RelativeLayout.ALIGN_BOTTOM, ivIcon.getId());
+        tvQuoteParams.addRule(RelativeLayout.ALIGN_START, tvTitle.getId());
+        tvQuoteParams.addRule(RelativeLayout.BELOW, tvTitle.getId());
+        tvQuoteParams.setMargins(0, getResources().getDimensionPixelSize(R.dimen.eight_dp), 0, 0);
+        relativeLayout.addView(tvQuote, tvQuoteParams);
+
+        EditText etComment = new EditText(this);
+        etComment.setInputType(InputType.TYPE_CLASS_TEXT);
+        etComment.setId(111);
+        RelativeLayout.LayoutParams etCommentParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        etCommentParams.setMargins(0, getResources().getDimensionPixelSize(R.dimen.eight_dp), 0, 0);
+        etComment.setHint(getResources().getString(R.string.etcomment));
+        etComment.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
+        etCommentParams.addRule(RelativeLayout.BELOW, ivIcon.getId());
+        etComment.setLayoutParams(etCommentParams);
+        relativeLayout.addView(etComment);
+
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.BELOW, etComment.getId());
+        linearLayout.setLayoutParams(layoutParams);
+        relativeLayout.addView(linearLayout);
+
+        Button btnPrevious = new Button(this);
+        LinearLayout.LayoutParams btnPreviousParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+        btnPrevious.setLayoutParams(btnPreviousParams);
+        btnPrevious.setText(getResources().getString(R.string.btn_previous));
+        linearLayout.addView(btnPrevious);
+
+        Button btnNext = new Button(this);
+        LinearLayout.LayoutParams btnNextParams = new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT, 1f);
+        btnNext.setLayoutParams(btnNextParams);
+        btnNext.setText(getResources().getString(R.string.btn_next));
+        linearLayout.addView(btnNext);
+
+        setContentView(root);
     }
 
 
